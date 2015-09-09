0e0e228716495f3725d700d6786eee569c6080f4
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-09-09 14:22:03 -0600

    Initial commit, basic CRUD functional

---
+++ controllers/topicCtrl.js
@@ -0,0 +1,50 @@
+var Topic = require('../models/TopicModel.js');
+
+
+
+module.exports = {
+
+	getTopic: function(req, res) {
+		Topic.find()
+		.populate('subjects.recipientGroup')
+		.exec().then(function(data, err) {
+			if (err) {
+				res.error(500).send(err);
+			} else {
+				res.send(data);
+			}
+		})
+	},
+
+	addTopic: function(req, res) {
+		new Topic(req.body).save(function(err, data) {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				res.send(data);
+			}
+		})
+	},
+
+	updateTopic: function(req, res) {
+		Topic.findByIdAndUpdate(req.query.id, req.body, function(err, data) {
+			if (err) {
+				res.error(500).send('broken');
+			} else {
+				res.send(data)
+			}
+		})
+	},
+
+	deleteTopic: function(req, res) {
+		Topic.findByIdAndRemove(req.query.id).then(function(err, data) {
+			if (err) {
+				res.error(500).send('broken');
+			} else {
+				res.send(data);
+			}
+		})
+
+	}
+
+}
---
+++ models/TopicModel.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	RecipientGroups = require('./RecipientGroupsModel.js');
+
+var TopicSchema = new Schema({
+
+	topicName: String,
+	subjects: [{
+		subjectName: String,
+		date: Date,
+		id: String,
+		recipientGroup: { type: mongoose.Schema.Types.ObjectId, ref: 'RecipientGroup' },
+		results: [{
+			// username: recipientGroup.users.name, //copied from recipient groups
+			// userEmail: recipientGroup.users.email, //copied from recipient groups
+			answers: [{
+				question: String,
+				answer: String
+			}]
+		}]
+	}]
+
+});
+
+module.exports = mongoose.model('Topic', TopicSchema);
+
