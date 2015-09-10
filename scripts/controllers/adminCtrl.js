7cb49206e58471c6118573bb68ecb83bdd891e7e
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-09-10 15:40:04 -0600

    Survey Template creation working, front and back end

--- scripts/controllers/adminCtrl.js
+++ scripts/controllers/adminCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('topicSurvey');
 
-app.controller('adminCtrl', function($scope, $location) {
+app.controller('adminCtrl', function($scope, $location, homeService) {
 
 	$scope.adminRedirect = function(location) {
 		$location.url(location);
@@ -9,14 +9,32 @@ app.controller('adminCtrl', function($scope, $location) {
 	$scope.answerContainer = [''];
 	$scope.varNameContainer = [''];
 
-	$scope.addNewInput = function( model ) {
-        $scope[ model ].push("");
+	$scope.addNewInput = function( array ) {
+        array.push("");
 	};
 
-	$scope.removeInput = function(index, model) {
-		console.log($scope[ model ]);
-		$scope[ model ].splice(index, 1)
-		console.log($scope[ model ]);
+	$scope.removeInput = function(index, array) {
+		array.splice(index, 1);
 	}
+
+	$scope.addQuestion = function() {
+		$scope.questions.push({
+			titleText: '',
+			helpText: '',
+			answers: [''],
+			questionType: ''
+		})
+	}
+
+	$scope.postSurveyTemplate = function(name, description, questions, varNames) {
+		homeService.postSurveyTemplate(name, description, questions, varNames);
+	}
+
+	$scope.questions = [{
+		titleText: '',
+		helpText: '',
+		answers: [''],
+		questionType: ''
+	}]
 
 });
--- scripts/services/homeService.js
+++ scripts/services/homeService.js
@@ -1,5 +1,23 @@
 var app = angular.module('topicSurvey');
 
-app.service('homeService', function() {
+app.service('homeService', function($http) {
+
+	
+	this.postSurveyTemplate = function(name, description, questions, varNames) {
+
+		var newSurvey = new SurveyTemplate(name, description, questions, varNames);
+
+		$http.post('http://0.0.0.0:8000/api/surveyTemplates', newSurvey)
+			.then(function(response) {
+				console.log(response);
+			})
+		}
 
 });
+
+SurveyTemplate = function(name, description, questions, varNames) {
+	this.name = name;
+	this.description = description;
+	this.questions = questions;
+	this.varNames = varNames;
+}
