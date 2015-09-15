b382f55d1d15b595f9a7113f596ddc41a78a0c27
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-09-15 13:58:54 -0600

    Changed directive to use templateUrl for readability

--- scripts/directives/questionTypeDir.js
+++ scripts/directives/questionTypeDir.js
@@ -7,16 +7,16 @@ app.directive('questionType', function() {
 			question: '='
 		},
 
-		template: '<input name="question.titleText" ng-repeat="answer in question.answers" ng-if="question.questionType === \'multiple-choice\'" type="radio"><input name="question.titleText" ng-if="question.questionType === \'select-many\'" ng-repeat="answer in question.answers" type="checkbox"><input name="titleText" ng-if="question.questionType === \'text\'" ng-repeat="answer in question.answers" type="text">'
+		templateUrl: '../templates/questionTypeTmpl.html'
 	}
 });
 
-		// link: function(scope, elem, attrs) {
-		// 	if (scope.question.questionType === 'multiple-choice') {
-		// 		return '<input ng-repeat="answer in question.answers" type="radio">';
-		// 	} else if (scope.question.questionType === 'select-many') {
-		// 		return '<input ng-repeat="answer in question.answers" type="checkbox">';
-		// 	} else if (scope.question.questionType === 'text') {
-		// 		return '<input ng-repeat="answer in question.answers" type="text">';
-		// 	}
-		// }
+// link: function(scope, elem, attrs) {
+// 	if (scope.question.questionType === 'multiple-choice') {
+// 		return '<input ng-repeat="answer in question.answers" type="radio">';
+// 	} else if (scope.question.questionType === 'select-many') {
+// 		return '<input ng-repeat="answer in question.answers" type="checkbox">';
+// 	} else if (scope.question.questionType === 'text') {
+// 		return '<input ng-repeat="answer in question.answers" type="text">';
+// 	}
+// }
